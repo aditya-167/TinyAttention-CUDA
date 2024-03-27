2a63252f1abfc2791f248eca974b32d08a9d9437
Kilian Haefeli <44493092+kilian888@users.noreply.github.com>
2024-03-26 20:59:05 -0400

    init

---
+++ attention.cu
@@ -0,0 +1,4 @@
+// assume row wise layout called from pytorch via pybind
+// Eunjin: Transpose kernel call fucntion for pybind
+// Aditya: Softmax Kernel and call function
+// Kilian: Matrix Multiplication
