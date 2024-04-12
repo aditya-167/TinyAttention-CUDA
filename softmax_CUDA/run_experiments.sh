b72dd86c406282fedf8345094c8440c7bbe1e8cc
kilianhae <konstantinhae@gmail.com>
2024-04-12 21:27:16 +0200

    run_experiments_ softmax

---
+++ softmax_CUDA/run_experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Compile and run all .cu files in the current directory except specified ones
+for file in *.cu; do
+    if [ -f "$file" ] && [ "$file" != "softmaxCuda_main_experiments.cu" ] && [ "$file" != "softmaxCuda_CuDNN.cu" ]; then
+        filename="${file%.*}"  # Extract filename without extension
+        echo "Compiling $file..."
+        nvcc "$file" -o "$filename"
+        if [ $? -eq 0 ]; then
+            echo "Running $filename..."
+            ./"$filename"
+            echo "-------------------------------------"
+        else
+            echo "Compilation failed for $file"
+        fi
+    fi
+done
